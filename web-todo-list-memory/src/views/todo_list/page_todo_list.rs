797a936138f7c5f29b05279526f6d691774e6148
Andres Giraldo <andresganc@gmail.com>
2024-11-03 12:18:39 -0500

    Mod: Web Todo List Memory: Section Header Todo List

--- web-todo-list-memory/src/views/todo_list/page_todo_list.rs
+++ web-todo-list-memory/src/views/todo_list/page_todo_list.rs
@@ -2,9 +2,13 @@
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
 
+// MODULES
+use super::section_header_tl::SectionHeaderTL;
+
 #[component]
 pub fn PageTodoList() -> Element {
     rsx! {
         h2 {"PAGE TODO LIST"}
+        {SectionHeaderTL()}
     }
 }
---
+++ web-todo-list-memory/src/views/todo_list/section_header_tl.rs
@@ -0,0 +1,10 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+#[component]
+pub fn SectionHeaderTL() -> Element {
+    rsx! {
+        h3{"Section Header TL"}
+    }
+}
