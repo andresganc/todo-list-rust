756130375bac6d1135807b3c42fe81a1d2bae01e
Andres Giraldo <andresganc@gmail.com>
2024-11-04 23:56:39 -0500

    Mod: Web Todo List Memory: Section Footer TL

---
+++ web-todo-list-memory/src/views/todo_list/section_footer_tl.rs
@@ -0,0 +1,12 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+#[component]
+pub fn SectionFooterTL() -> Element {
+    rsx! {
+        div {
+            h3 {"Section Footer TL"}
+        }
+    }
+}
