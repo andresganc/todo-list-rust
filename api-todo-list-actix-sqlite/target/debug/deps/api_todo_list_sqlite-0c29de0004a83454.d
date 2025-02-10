3014ac8b8e04edf17a5492d2c578d3533feb3273
Andres Giraldo <andresganc@gmail.com>
2025-02-10 12:37:21 -0500

    New: CLI Todo List

---
+++ api-todo-list-actix-sqlite/target/debug/deps/api_todo_list_sqlite-0c29de0004a83454.d
@@ -0,0 +1,5 @@
+/home/andres/NC/ANDRES/GITHUB/todo-list-rust/api-todo-list-actix-sqlite/target/debug/deps/libapi_todo_list_sqlite-0c29de0004a83454.rmeta: src/main.rs
+
+/home/andres/NC/ANDRES/GITHUB/todo-list-rust/api-todo-list-actix-sqlite/target/debug/deps/api_todo_list_sqlite-0c29de0004a83454.d: src/main.rs
+
+src/main.rs:
---
+++ api-todo-list-actix-sqlite/target/debug/deps/api_todo_list_sqlite-f8a1e22adc28b36c.d
@@ -0,0 +1,5 @@
+/home/andres/NC/ANDRES/GITHUB/todo-list-rust/api-todo-list-actix-sqlite/target/debug/deps/libapi_todo_list_sqlite-f8a1e22adc28b36c.rmeta: src/main.rs
+
+/home/andres/NC/ANDRES/GITHUB/todo-list-rust/api-todo-list-actix-sqlite/target/debug/deps/api_todo_list_sqlite-f8a1e22adc28b36c.d: src/main.rs
+
+src/main.rs:
